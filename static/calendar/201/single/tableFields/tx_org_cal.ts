f3b53cceb6618c2a3fbb593cc7a76448769379f0
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-17 23:24:32 +0000

    6.0.7 +++ Upgrade Org 6.0.7
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org_repertoire/trunk@87603 735d13b6-9817-0410-8766-e36946ffe9aa

--- static/calendar/201/single/tableFields/tx_org_cal.ts
+++ static/calendar/201/single/tableFields/tx_org_cal.ts
@@ -2,39 +2,18 @@ plugin.tx_browser_pi1 {
   views {
     single {
       201 {
-          // 140706: empty statement: for proper comments only
-        tx_org_cal {
-        }
-          // title: image, header, bodytext
         tx_org_cal =
         tx_org_cal {
           title < plugin.tx_browser_pi1.displaySingle.master_templates.tableFields.imageText.0
           title {
             20 {
               0 {
-                10 >
-                  // bookmarks, header, bodytext
-                10 = COA
                 10 {
-                    // socialmedia_bookmarks
-                  10 = TEXT
-                  10 {
-                    value = ###SOCIALMEDIA_BOOKMARKS###
-                    wrap = <div class="show-for-large-up socialbookmarks">|</div>
-                  }
-                    // header
-                  20 = TEXT
                   20 {
-                    field = tx_org_cal.title
-                    wrap = <h1>|</h1>
+                    field := prependString(tx_org_repertoire.title // )
                   }
-                    // bodytext
-                  30 = TEXT
                   30 {
-                    field = tx_org_cal.bodytext
-                    stdWrap {
-                      parseFunc < lib.parseFunc_RTE
-                    }
+                    field := prependString(tx_org_repertoire.bodytext // )
                   }
                 }
               }
