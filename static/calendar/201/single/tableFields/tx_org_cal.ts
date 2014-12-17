a1ae6224703985bb2976a3f5c0ee524bf40e3334
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-17 23:38:05 +0000

    6.0.7 +++ Upgrade Org 6.0.7
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org_repertoire/trunk@87606 735d13b6-9817-0410-8766-e36946ffe9aa

--- static/calendar/201/single/tableFields/tx_org_cal.ts
+++ static/calendar/201/single/tableFields/tx_org_cal.ts
@@ -12,29 +12,12 @@ plugin.tx_browser_pi1 {
           title {
             20 {
               0 {
-                10 >
-                  // bookmarks, header, bodytext
-                10 = COA
                 10 {
-                    // socialmedia_bookmarks
-                  10 = TEXT
-                  10 {
-                    value = ###SOCIALMEDIA_BOOKMARKS###
-                    wrap = <div class="show-for-large-up socialbookmarks">|</div>
-                  }
-                    // header
-                  20 = TEXT
                   20 {
-                    field = tx_org_cal.title
-                    wrap = <h1>|</h1>
+                    field = := prependString(tx_org_repertoire.title // )
                   }
-                    // bodytext
-                  30 = TEXT
                   30 {
-                    field = tx_org_cal.bodytext
-                    stdWrap {
-                      parseFunc < lib.parseFunc_RTE
-                    }
+                    field = := prependString(tx_org_repertoire.bodytext // )
                   }
                 }
               }
@@ -73,59 +56,6 @@ plugin.tx_browser_pi1 {
               26 {
                 10 >
                 10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
-              }
-            }
-          }
-            // margin: datesheet
-          datetime = COA
-          datetime {
-            wrap = |
-            10 = COA
-            10 {
-              if {
-                isTrue {
-                  field = tx_org_cal.datetime
-                }
-              }
-                // 10: date isn't expired
-              10 = COA
-              10 {
-                if {
-                  value {
-                    data = date : U
-                  }
-                  isGreaterThan {
-                    field = tx_org_cal.datetime
-                  }
-                }
-                wrap = <ul class="vcard datesheet">|</ul><!-- vcard -->
-                  // name of weekday
-                10 = TEXT
-                10 {
-                  field = tx_org_cal.datetime
-                  strftime  = %a
-                  wrap = <li class="weekday">|</li>
-                }
-                  // day of month as number
-                20 < .10
-                20 {
-                  strftime  = %d
-                  wrap      = <li class="day_of_month">|</li>
-                }
-                  // month year
-                30 < .10
-                30 {
-                  strftime  = %b %y
-                  wrap      = <li class="month">|</li>
-                }
-              }
-                // 20: date is expired
-              20 < .10
-              20 {
-                if {
-                  negate = 1
-                }
-                wrap = <ul class="vcard datesheet datesheet_expired">|</ul><!-- vcard -->
               }
             }
           }
