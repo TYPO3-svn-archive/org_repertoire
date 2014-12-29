84ed239f0c0734c28f8bf194f1b3b01f5717ad70
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-29 17:30:34 +0000

    6.0.7 +++ 201
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org_repertoire/trunk@87889 735d13b6-9817-0410-8766-e36946ffe9aa

--- static/calendar/201/single/tableFields/_setup.ts
+++ static/calendar/201/single/tableFields/_setup.ts
@@ -1,3 +1,3 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:org_repertoire/static/calendar/201/single/tableFields/tx_org_cal.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:org_repertoire/static/calendar/201/single/tableFields/tx_org_calentrance.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:org_repertoire/static/calendar/201/single/tableFields/tx_org_location.ts">
+//<INCLUDE_TYPOSCRIPT: source="FILE:EXT:org_repertoire/static/calendar/201/single/tableFields/tx_org_calentrance.ts">
+//<INCLUDE_TYPOSCRIPT: source="FILE:EXT:org_repertoire/static/calendar/201/single/tableFields/tx_org_location.ts">
--- static/calendar/201/single/tableFields/tx_org_cal.ts
+++ static/calendar/201/single/tableFields/tx_org_cal.ts
@@ -2,47 +2,19 @@ plugin.tx_browser_pi1 {
   views {
     single {
       201 {
-          // 140706: empty statement: for proper comments only
         tx_org_cal {
-        }
-          // title: image, header, bodytext
-        tx_org_cal =
-        tx_org_cal {
-          title < plugin.tx_browser_pi1.displaySingle.master_templates.tableFields.imageText.0
           title {
             20 {
               0 {
-                10 >
-                  // bookmarks, header, bodytext
-                10 = COA
                 10 {
-                    // socialmedia_bookmarks
-                  10 = TEXT
-                  10 {
-                    value = ###SOCIALMEDIA_BOOKMARKS###
-                    wrap = <div class="show-for-large-up socialbookmarks">|</div>
-                  }
-                    // header
-                  20 = TEXT
                   20 {
-                    field = tx_org_repertoire.title // tx_org_cal.bodytext
-                    wrap = <h1>|</h1>
+                    field := prependString(tx_org_repertoire.title // )
                   }
-                    // subtitle
-                  21 = TEXT
                   21 {
-                    field = tx_org_repertoire.subtitle // tx_org_cal.subtitle
-                    wrap = <h2>|</h2>
-                    required = 1
+                    field := prependString(tx_org_repertoire.subtitle // )
                   }
-                    // bodytext
-                  30 = TEXT
                   30 {
-                    field = tx_org_repertoire.bodytext // tx_org_cal.bodytext
-                    required = 1
-                    stdWrap {
-                      parseFunc < lib.parseFunc_RTE
-                    }
+                    field := prependString(tx_org_repertoire.bodytext // )
                   }
                 }
               }
