c29baefae3c6f5b1b4b2b880db21bfadfb348432
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-01-03 12:40:57 +0000

    6.0.7 +++ 201 repertoire
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org_repertoire/trunk@88089 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/Org/Calendar/201/list/tableFields.ts
+++ Configuration/TypoScript/Org/Calendar/201/list/tableFields.ts
@@ -5,9 +5,6 @@ plugin.tx_browser_pi1 {
         tx_org_cal {
           title {
             10 {
-              20 {
-                tx_org_repertoire <  plugin.tx_browser_pi1.views.list.201.tx_org_cal.title.10.20.tx_org_event
-              }
                 // tx_org_cal.subtitle
               21 {
                 field := prependString(tx_org_repertoire.teaser_subtitle // tx_org_repertoire.subtitle // )
