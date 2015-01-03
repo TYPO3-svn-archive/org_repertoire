cd0694d48c4c36f233809487839e04eaaa137f2c
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-01-03 12:27:11 +0000

    6.0.7 +++ 201 repertoire
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org_repertoire/trunk@88087 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/Org/Calendar/201/list/tableFields.ts
+++ Configuration/TypoScript/Org/Calendar/201/list/tableFields.ts
@@ -5,34 +5,12 @@ plugin.tx_browser_pi1 {
         tx_org_cal {
           title {
             10 {
-              XXX20 {
-                  // link to tx_org_repertoire
-                tx_org_repertoire < .default
+              20 {
+                tx_org_repertoire <  plugin.tx_browser_pi1.views.list.201.tx_org_cal.title.10.20.tx_org_event
               }
                 // tx_org_cal.subtitle
               21 {
                 field := prependString(tx_org_repertoire.teaser_subtitle // tx_org_repertoire.subtitle // )
-              }
-              XXX30 >
-                // image
-              XXX39 {
-                10 {
-                  tx_org_repertoire < .default
-                  tx_org_repertoire {
-                    file.import.stdWrap.cObject.10.20.field := prependString(tx_org_repertoire.image // )
-                    altText.field := prependString(tx_org_repertoire.imageseo // )
-                    title.field := prependString(tx_org_repertoire.imageseo // )
-                  }
-                }
-              }
-              XXX40 {
-                  // link to tx_org_repertoire
-                tx_org_repertoire < .default
-                tx_org_repertoire {
-                  10 {
-                    field := prependString(tx_org_repertoire.teaser_short // tx_org_repertoire.bodytext // )
-                  }
-                }
               }
             }
           }
