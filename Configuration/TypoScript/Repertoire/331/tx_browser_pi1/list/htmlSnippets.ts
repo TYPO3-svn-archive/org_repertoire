0f9feb7305dde533294895df08192fb436f9f8ab
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-01-03 13:09:56 +0000

    6.0.7 +++ 331 new
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org_repertoire/trunk@88092 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/Repertoire/331/tx_browser_pi1/list/htmlSnippets.ts
+++ Configuration/TypoScript/Repertoire/331/tx_browser_pi1/list/htmlSnippets.ts
@@ -22,16 +22,8 @@ plugin.tx_browser_pi1 {
                   <div id="c###TT_CONTENT.UID###-listview-###MODE###" class="columns listview listview-content listview-###MODE### listview-content-###MODE###">
                     <!-- ###LISTBODY### begin --><!-- ###LISTBODYITEM### begin -->
                     <div class="row">
-                      <div class="columns record">
-                        <div class="image01">
-                          ###TX_ORG_REPERTOIRE.IMAGE###
-                        </div>
-                        <div class="sbmFloatRight">
-                          ###SOCIALMEDIA_BOOKMARKS###
-                        </div>
-                        ###TX_ORG_REPERTOIRE.SUBTITLE###
+                      <div class="record">
                         ###TX_ORG_REPERTOIRE.TITLE###
-                        ###TX_ORG_REPERTOIRE.TEASER_SHORT###
                       </div>
                     </div><!-- /row --><!-- ###LISTBODYITEM### end --><!-- ###LISTBODY### end -->
                   </div><!-- /columns --><!-- /listview -->
--- Configuration/TypoScript/Repertoire/331/tx_browser_pi1/list/tableFields.ts
+++ Configuration/TypoScript/Repertoire/331/tx_browser_pi1/list/tableFields.ts
@@ -2,6 +2,254 @@ plugin.tx_browser_pi1 {
   views {
     list {
       331 {
+          // title
+        tx_org_repertoire =
+        tx_org_repertoire {
+            // bookmarks, title, subtitle, tx_org_repertoiretype.title, image, teaser_short, location, datesheet
+          title = COA
+          title {
+              // bookmarks, title, subtitle, tx_org_repertoiretype.title, image, teaser_short, location
+            10 = COA
+            10 {
+                // socialmedia_bookmarks
+              10 = TEXT
+              10 {
+                value = ###SOCIALMEDIA_BOOKMARKS###
+                wrap = <div style="float:right;">|</div>
+              }
+                // title: default, notype, page, url, tx_org_event
+              20 < plugin.tx_browser_pi1.displayList.master_templates.tableFields.header.0
+              20 {
+                //tx_org_event < plugin.tx_browser_pi1.displayList.master_templates.tableFields.header.6
+              }
+                // subtitle
+              21 = TEXT
+              21 {
+                field = tx_org_repertoire.teaser_subtitle // tx_org_repertoire.subtitle
+                wrap = <h3>|</h3>
+                required = 1
+              }
+                // tx_org_repertoiretype.title
+              30 = COA
+              30 {
+                if {
+                  isTrue {
+                    field = tx_org_repertoiretype.title
+                  }
+                }
+                wrap = <p class="tx_org_repertoiretype">|</p>
+                  // label
+                10 = TEXT
+                10 {
+                  data = LLL:EXT:org/locallang_db.xml:filter_phrase.caltype.title
+                  noTrimWrap = ||: |
+                }
+                20 = TEXT
+                20 {
+                  field = tx_org_repertoiretype.title
+                }
+              }
+                // image
+              39 = COA
+              39 {
+                  // image
+                10 < plugin.tx_browser_pi1.displayList.master_templates.tableFields.image.0
+                10 {
+                  //tx_org_event < plugin.tx_browser_pi1.displayList.master_templates.tableFields.image.6
+                }
+                wrap = <div style="float:left;padding-right:1em;">|</div>
+              }
+                // teaser_short: default, notype, page, url, tx_org_event
+              40 < plugin.tx_browser_pi1.displayList.master_templates.tableFields.text.0
+              40 {
+                //tx_org_event < plugin.tx_browser_pi1.displayList.master_templates.tableFields.text.6
+              }
+              wrap = <div class="columns small-12 medium-12 large-10">|</div>
+            }
+              // margin: datesheet
+            20 = COA
+            20 {
+              wrap = <div class="columns small-12 medium-12 large-2">|</div>
+              10 = COA
+              10 {
+                if {
+                  isTrue {
+                    field = tx_org_cal.datetime
+                  }
+                }
+                  // 10: date isn't expired
+                10 = COA
+                10 {
+                  if {
+                    value {
+                      data = date : U
+                    }
+                    isGreaterThan {
+                      field = tx_org_cal.datetime
+                    }
+                  }
+                  wrap = <ul class="vcard datesheet">|</ul><!-- vcard -->
+                    // name of weekday
+                  10 < plugin.tx_browser_pi1.displayList.master_templates.tableFields.details.0
+                  10 {
+                    default {
+                      value >
+                      lang >
+                      field = tx_org_cal.datetime
+                      strftime  = %a
+                      wrap = <li class="weekday">|</li>
+                    }
+                    notype {
+                      value >
+                      lang >
+                      field = tx_org_cal.datetime
+                      strftime  = %a
+                      wrap = <li class="weekday">|</li>
+                    }
+                    page {
+                      value >
+                      lang >
+                      field = tx_org_cal.datetime
+                      strftime  = %a
+                      wrap = <li class="weekday">|</li>
+                    }
+                    tx_org_event < plugin.tx_browser_pi1.displayList.master_templates.tableFields.details.6
+                    tx_org_event {
+                      default {
+                        value >
+                        lang >
+                        field = tx_org_cal.datetime
+                        strftime  = %a
+                        wrap = <li class="weekday">|</li>
+                      }
+                      notype {
+                        value >
+                        lang >
+                        field = tx_org_cal.datetime
+                        strftime  = %a
+                        wrap = <li class="weekday">|</li>
+                      }
+                      page {
+                        value >
+                        lang >
+                        field = tx_org_cal.datetime
+                        strftime  = %a
+                        wrap = <li class="weekday">|</li>
+                      }
+                      url {
+                        value >
+                        lang >
+                        field = tx_org_cal.datetime
+                        strftime  = %a
+                        wrap = <li class="weekday">|</li>
+                      }
+                    }
+                    url {
+                      value >
+                      lang >
+                      field = tx_org_cal.datetime
+                      strftime  = %a
+                      wrap = <li class="weekday">|</li>
+                    }
+                  }
+                    // day of month as number
+                  20 < .10
+                  20 {
+                    default {
+                      strftime  = %d
+                      wrap      = <li class="day_of_month">|</li>
+                    }
+                    notype {
+                      strftime  = %d
+                      wrap      = <li class="day_of_month">|</li>
+                    }
+                    page {
+                      strftime  = %d
+                      wrap      = <li class="day_of_month">|</li>
+                    }
+                    tx_org_event {
+                      default {
+                        strftime  = %d
+                        wrap      = <li class="day_of_month">|</li>
+                      }
+                      notype {
+                        strftime  = %d
+                        wrap      = <li class="day_of_month">|</li>
+                      }
+                      page {
+                        strftime  = %d
+                        wrap      = <li class="day_of_month">|</li>
+                      }
+                      url {
+                        strftime  = %d
+                        wrap      = <li class="day_of_month">|</li>
+                      }
+                    }
+                    url {
+                      strftime  = %d
+                      wrap      = <li class="day_of_month">|</li>
+                    }
+                  }
+                    // month year
+                  30 < .10
+                  30 {
+                    default {
+                      strftime  = %b %y
+                      wrap      = <li class="month">|</li>
+                    }
+                    notype {
+                      strftime  = %b %y
+                      wrap      = <li class="month">|</li>
+                    }
+                    page {
+                      strftime  = %b %y
+                      wrap      = <li class="month">|</li>
+                    }
+                    tx_org_event {
+                      default {
+                        strftime  = %b %y
+                        wrap      = <li class="month">|</li>
+                      }
+                      notype {
+                        strftime  = %b %y
+                        wrap      = <li class="month">|</li>
+                      }
+                      page {
+                        strftime  = %b %y
+                        wrap      = <li class="month">|</li>
+                      }
+                      url {
+                        strftime  = %b %y
+                        wrap      = <li class="month">|</li>
+                      }
+                    }
+                    url {
+                      strftime  = %b %y
+                      wrap      = <li class="month">|</li>
+                    }
+                  }
+                }
+                  // 20: date is expired
+                20 < .10
+                20 {
+                  if {
+                    negate = 1
+                  }
+                  wrap = <ul class="vcard datesheet datesheet_expired">|</ul><!-- vcard -->
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+XXXplugin.tx_browser_pi1 {
+  views {
+    list {
+      331 {
         tx_org_cal {
           datetime = COA
           datetime {
