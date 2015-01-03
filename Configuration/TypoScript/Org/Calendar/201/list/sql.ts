b3e5cf163a2c4ec503dc5b315363eaae46e74111
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-01-03 10:57:04 +0000

    6.0.7 +++ 201 cal
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org_repertoire/trunk@88073 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/Org/Calendar/201/list/sql.ts
+++ Configuration/TypoScript/Org/Calendar/201/list/sql.ts
@@ -4,7 +4,7 @@ plugin.tx_browser_pi1 {
       201 = +Org-Repertoire: Calendar
       201 {
         name    = +Org-Repertoire: Calendar
-        select := addToList(tx_org_repertoire.title,tx_org_repertoire.subtitle,tx_org_repertoire.producer,tx_org_repertoire.teaser_subtitle,tx_org_repertoire.teaser_short,tx_org_repertoire.teaser_title,tx_org_repertoire.bodytext)
+        select := addToList(tx_org_repertoire.title,tx_org_repertoire.image,tx_org_repertoire.imageseo,tx_org_repertoire.subtitle,tx_org_repertoire.producer,tx_org_repertoire.teaser_subtitle,tx_org_repertoire.teaser_short,tx_org_repertoire.teaser_title,tx_org_repertoire.bodytext,tx_org_repertoire.page,tx_org_repertoire.type,tx_org_repertoire.url)
       }
     }
   }
