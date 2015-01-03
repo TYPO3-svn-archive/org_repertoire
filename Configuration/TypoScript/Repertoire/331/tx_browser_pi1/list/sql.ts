dad3c884ebd7db0d5f8bf073ddb6178d632f2132
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-01-03 01:11:28 +0000

    6.0.7 +++ default icon
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org_repertoire/trunk@88064 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/Repertoire/331/tx_browser_pi1/list/sql.ts
+++ Configuration/TypoScript/Repertoire/331/tx_browser_pi1/list/sql.ts
@@ -7,13 +7,10 @@ plugin.tx_browser_pi1 {
           tx_org_repertoire.bodytext,
           tx_org_repertoire.image,
           tx_org_repertoire.imageseo,
-          tx_org_repertoire.page,
           tx_org_repertoire.producer,
           tx_org_repertoire.subtitle,
           tx_org_repertoire.teaser_short,
-          tx_org_repertoire.type,
           tx_org_repertoire.uid,
-          tx_org_repertoire.url,
           tx_org_cal.datetime,
           tx_org_cal.type
         )
