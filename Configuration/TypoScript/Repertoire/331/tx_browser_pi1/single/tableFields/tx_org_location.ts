892626bd1a26e4a2907fc8969e582c9984a5f39f
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-10-23 23:08:21 +0000

    6.1.0 +++ Repertoire template: calender items with location name and location city
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org_repertoire/trunk@90001 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/Repertoire/331/tx_browser_pi1/single/tableFields/tx_org_location.ts
+++ Configuration/TypoScript/Repertoire/331/tx_browser_pi1/single/tableFields/tx_org_location.ts
@@ -46,7 +46,7 @@ plugin.tx_browser_pi1 {
                           }
                         }
                         if {
-                          isTrue = {$plugin.org-repertoire.templates.repertoire.plusLocation}
+                          isTrue = {$plugin.org.templates.repertoire.plusLocation}
                         }
                       }
                     }
