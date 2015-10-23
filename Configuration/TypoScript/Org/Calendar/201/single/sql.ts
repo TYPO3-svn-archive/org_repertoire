1a86cb0b10ca7cea295ac549ea348bb218a9ec6b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-10-23 14:59:54 +0000

    6.0.9 +++ Bugfix: image_zoom
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org_repertoire/trunk@89968 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/Org/Calendar/201/single/sql.ts
+++ Configuration/TypoScript/Org/Calendar/201/single/sql.ts
@@ -4,7 +4,7 @@ plugin.tx_browser_pi1 {
       201 = +Org-Repertoire: Calendar
       201 {
         name    = +Org-Repertoire: Calendar
-        select := addToList(tx_org_repertoire.title,tx_org_repertoire.bodytext,tx_org_repertoire.image,tx_org_repertoire.imageheight,tx_org_repertoire.imagewidth,tx_org_repertoire.imageseo,tx_org_repertoire.imagecaption,tx_org_repertoire.imageorient,tx_org_repertoire.imagecols,tx_org_repertoire.length,tx_org_repertoire.subtitle,tx_org_repertoire.producer,tx_org_repertoire.teaser_subtitle,tx_org_repertoire.teaser_short,tx_org_repertoire.teaser_title)
+        select := addToList(tx_org_repertoire.title,tx_org_repertoire.bodytext,tx_org_repertoire.image,tx_org_repertoire.imageheight,tx_org_repertoire.imagewidth,tx_org_repertoire.imageseo,tx_org_repertoire.imagecaption,tx_org_repertoire.imageorient,tx_org_repertoire.imagecols,tx_org_repertoire.image_zoom,tx_org_repertoire.length,tx_org_repertoire.subtitle,tx_org_repertoire.producer,tx_org_repertoire.teaser_subtitle,tx_org_repertoire.teaser_short,tx_org_repertoire.teaser_title)
       }
     }
   }
